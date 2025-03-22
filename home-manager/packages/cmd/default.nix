f632087fbc87bceafe73e2ae1cedbfb1c454d8a1
otetoteto <oteto2oteto@gmail.com>
2025-03-23 01:48:55 +0900

    add home-manager cmd: lsd & bat

--- home-manager/packages/cmd/default.nix
+++ home-manager/packages/cmd/default.nix
@@ -2,12 +2,14 @@
 {
   imports = [
     ./awscli.nix
+    ./bat.nix
     ./deno.nix
     ./fzf.nix
     ./gh.nix
     ./ghq.nix
     ./git.nix
     ./jq.nix
+    ./lsd.nix
     ./mise.nix
     ./rust.nix
     ./uv.nix
